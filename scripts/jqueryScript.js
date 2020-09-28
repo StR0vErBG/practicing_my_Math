83c13ece29921e419d563012ff75a6cdc183935b
StR0vErBG <xcreaw1@abv.bg>
2020-09-28 23:38:09 +0300

    mainPage

--- scripts/jqueryScript.js
+++ scripts/jqueryScript.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-  $("#menu").hide();
-  $("#menu").fadeIn(2000);
-});
+// $(document).ready(function () {
+//   $("#menu").hide();
+//   $("#menu").fadeIn(2000);
+// });
 
-$("#menu").click(function () {
-  $("body").fadeOut(2000);
-});
+// $("#menu").click(function () {
+//   $("body").fadeOut(2000);
+// });
